b8e839dfe5cc2da664d251eb217db91cb23be7e3
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-10-05 00:00:17 +0300

    deleted files

--- app/helpers/approvements_helper.rb
+++
@@ -1,2 +0,0 @@
-module ApprovementsHelper
-end
--- app/models/approvement.rb
+++
@@ -1,8 +0,0 @@
-class Approvement < ApplicationRecord
-  belongs_to :task
-  belongs_to :user
-
-  validates :task, presence: true
-
-  validates :user, uniqueness: { scope: :task_id }
-end
