91eac28ee922b62d92128e2f37c10dbcd7a02720
Alina Karzhavina <malina@MacBook-Air-Alina.local>
2021-10-05 19:00:01 +0300

    name field modified

---
+++ db/migrate/20211005113447_refactor_user_name.rb
@@ -0,0 +1,5 @@
+class RefactorUserName < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :users, :username, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_180222) do
+ActiveRecord::Schema.define(version: 2021_10_05_113447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_09_29_180222) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.string "email", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
